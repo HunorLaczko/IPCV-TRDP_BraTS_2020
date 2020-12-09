df927d99f53dc11e84999200a1e43738429b673b
Hunor Laczko <luni741@gmail.com>
2020-12-09 23:11:49 +0100

    Adding attention module
    Code from: https://github.com/kobiso/CBAM-keras

--- src/unet3d/model/isensee2017.py
+++ src/unet3d/model/isensee2017.py
@@ -1,8 +1,9 @@
 from functools import partial
 
-from keras.layers import Input, LeakyReLU, Add, UpSampling3D, Activation, SpatialDropout3D, Conv3D
+from keras.layers import Input, LeakyReLU, Add, UpSampling3D, Activation, SpatialDropout3D, Conv3D, GlobalAveragePooling3D, GlobalMaxPooling3D, Dense, Reshape, Permute, multiply, Concatenate, Lambda
 from keras.engine import Model
 from keras.optimizers import Adam
+from keras import backend as K
 
 from .unet import create_convolution_block, concatenate
 from ..metrics import weighted_dice_coefficient_loss
@@ -51,6 +52,8 @@ def isensee2017_model(input_shape=(4, 128, 128, 128), n_base_filters=16, depth=5
         context_output_layer = create_context_module(in_conv, n_level_filters, dropout_rate=dropout_rate)
 
         summation_layer = Add()([in_conv, context_output_layer])
+        summation_layer = cbam_block(summation_layer, ratio = 8)
+
         level_output_layers.append(summation_layer)
         current_layer = summation_layer
 
@@ -100,4 +103,81 @@ def create_context_module(input_layer, n_level_filters, dropout_rate=0.3, data_f
     return convolution2
 
 
+def cbam_block(cbam_feature, ratio=8):
+	"""Contains the implementation of Convolutional Block Attention Module(CBAM) block.
+	As described in https://arxiv.org/abs/1807.06521.
+	"""
+	
+	cbam_feature = channel_attention(cbam_feature, ratio)
+	cbam_feature = spatial_attention(cbam_feature)
+	return cbam_feature
 
+def channel_attention(input_feature, ratio=8):
+	
+	channel_axis = 1 if K.image_data_format() == "channels_first" else -1
+	channel = input_feature.shape[channel_axis]
+	
+	shared_layer_one = Dense(channel//ratio,
+							 activation='relu',
+							 kernel_initializer='he_normal',
+							 use_bias=True,
+							 bias_initializer='zeros')
+	shared_layer_two = Dense(channel,
+							 kernel_initializer='he_normal',
+							 use_bias=True,
+							 bias_initializer='zeros')
+	
+	avg_pool = GlobalAveragePooling3D()(input_feature)    
+	avg_pool = Reshape((1,1,1,channel))(avg_pool)
+	assert avg_pool.shape[1:] == (1,1,1,channel)
+	avg_pool = shared_layer_one(avg_pool)
+	assert avg_pool.shape[1:] == (1,1,1,channel//ratio)
+	avg_pool = shared_layer_two(avg_pool)
+	assert avg_pool.shape[1:] == (1,1,1,channel)
+	
+	max_pool = GlobalMaxPooling3D()(input_feature)
+	max_pool = Reshape((1,1,1,channel))(max_pool)
+	assert max_pool.shape[1:] == (1,1,1,channel)
+	max_pool = shared_layer_one(max_pool)
+	assert max_pool.shape[1:] == (1,1,1,channel//ratio)
+	max_pool = shared_layer_two(max_pool)
+	assert max_pool.shape[1:] == (1,1,1,channel)
+	
+	cbam_feature = Add()([avg_pool,max_pool])
+	cbam_feature = Activation('sigmoid')(cbam_feature)
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((4, 1, 2, 3))(cbam_feature)
+	
+	return multiply([input_feature, cbam_feature])
+
+def spatial_attention(input_feature):
+	kernel_size = 7
+	
+	if K.image_data_format() == "channels_first":
+		channel = input_feature.shape[1]
+		cbam_feature = Permute((2,3,4,1))(input_feature)
+	else:
+		channel = input_feature.shape[-1]
+		cbam_feature = input_feature
+	
+	avg_pool = Lambda(lambda x: K.mean(x, axis=4, keepdims=True))(cbam_feature)
+	assert avg_pool.shape[-1] == 1
+	max_pool = Lambda(lambda x: K.max(x, axis=4, keepdims=True))(cbam_feature)
+	assert max_pool.shape[-1] == 1
+	concat = Concatenate(axis=4)([avg_pool, max_pool])
+	assert concat.shape[-1] == 2
+	cbam_feature = Conv3D(filters = 1,
+					kernel_size=kernel_size,
+					strides=1,
+					padding='same',
+					activation='sigmoid',
+					kernel_initializer='he_normal',
+					use_bias=False,
+                    data_format="channels_last")(concat)	
+	assert cbam_feature.shape[-1] == 1
+	
+	if K.image_data_format() == "channels_first":
+		cbam_feature = Permute((4, 1, 2, 3))(cbam_feature)
+		
+	return multiply([input_feature, cbam_feature])
